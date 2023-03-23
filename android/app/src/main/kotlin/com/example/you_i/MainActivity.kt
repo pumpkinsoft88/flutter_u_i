986e3a15844aa9c78d14f87fe49f8371f7d7c3b8
pumpkinsoft88 <pumpkinsoft88@gmail.com>
2023-03-24 06:22:52 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/you_i/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.you_i
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:you_i/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
+
