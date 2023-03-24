d771b990b65e8faddd33100d1cf7988638fda35e
pumpkinsoft88 <pumpkinsoft88@gmail.com>
2023-03-25 05:57:23 +0900

    3rd commit -state management simple

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime(
+    DateTime
+        .now()
+        .year,
+    DateTime
+        .now()
+        .month,
+    DateTime
+        .now()
+        .day,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +28,16 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: Container(
-          width: MediaQuery.of(context).size.width,
+          width: MediaQuery
+              .of(context)
+              .size
+              .width,
           child: Column(
             children: [
-              _TopPart(),
+              _TopPart(
+                selectedDate: selectedDate,
+                onPressed: onHeartPressed,
+              ),
               _BottomPart(),
             ],
           ),
@@ -22,21 +45,48 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+  void onHeartPressed() {
+    final DateTime now = DateTime.now();
+    // dialog
+    showCupertinoDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Colors.white,
+              height: 300.0,
+              child: CupertinoDatePicker(
+                mode: CupertinoDatePickerMode.date,
+                initialDateTime: selectedDate,
+                maximumDate: DateTime(
+                  now.year,
+                  now.month,
+                  now.day,
+                ),
+                onDateTimeChanged: (DateTime date) {
+                  setState(() {
+                    selectedDate = date;
+                  });
+                },
+              ),
+            ),
+          );
+        });
+  }
 }
 
-class _TopPart extends StatefulWidget {
-  const _TopPart({Key? key}) : super(key: key);
+class _TopPart extends StatelessWidget {
+  final DateTime selectedDate;
+  final VoidCallback onPressed;
 
-  @override
-  State<_TopPart> createState() => _TopPartState();
-}
-
-class _TopPartState extends State<_TopPart> {
-  DateTime selectedDate = DateTime(
-    DateTime.now().year,
-    DateTime.now().month,
-    DateTime.now().day,
-  );
+  _TopPart({
+    required this.selectedDate,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -46,84 +96,58 @@ class _TopPartState extends State<_TopPart> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
+        Text(
+        'U&I',
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: 'parisienne',
+          fontSize: 80.0,
+        ),
+      ),
+      Column(
+        children: [
           Text(
-            'U&I',
+            '우리 처음 만난 날',
             style: TextStyle(
               color: Colors.white,
-              fontFamily: 'parisienne',
-              fontSize: 80.0,
-            ),
-          ),
-          Column(
-            children: [
-              Text(
-                '우리 처음 만난 날',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 30.0,
-                ),
-              ),
-              Text(
-                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 20.0,
-                ),
-              ),
-            ],
-          ),
-          IconButton(
-            iconSize: 60,
-            onPressed: () {
-              // dialog
-              showCupertinoDialog(
-                  context: context,
-                  barrierDismissible: true,
-                  builder: (BuildContext context) {
-                    return Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        color: Colors.white,
-                        height: 300.0,
-                        child: CupertinoDatePicker(
-                          mode: CupertinoDatePickerMode.date,
-                          initialDateTime: selectedDate,
-                          maximumDate: DateTime(
-                            now.year,
-                            now.month,
-                            now.day,
-                          ),
-                          onDateTimeChanged: (DateTime date) {
-                            setState(() {
-                              selectedDate = date;
-                            });
-                          },
-                        ),
-                      ),
-                    );
-                  });
-            },
-            icon: Icon(
-              Icons.favorite,
-              color: Colors.red,
+              fontFamily: 'sunflower',
+              fontSize: 30.0,
             ),
           ),
           Text(
-            'D+${DateTime(
-                  now.year,
-                  now.month,
-                  now.day,
-                ).difference(selectedDate).inDays + 1}',
+            '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
             style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'sunflower',
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700),
-          )
+              color: Colors.white,
+              fontFamily: 'sunflower',
+              fontSize: 20.0,
+            ),
+          ),
         ],
       ),
+      IconButton(
+        iconSize: 60.0,
+        onPressed: onPressed,
+        icon: Icon(
+        Icons.favorite,
+        color: Colors.red,
+      ),
+    ),
+    Text(
+    'D+${DateTime(
+    now.year,
+    now.month,
+    now.day,
+    ).difference(selectedDate).inDays + 1}',
+    style: TextStyle(
+    color: Colors.white,
+    fontFamily: 'sunflower',
+    fontSize: 50.0,
+    fontWeight: FontWeight.w700),
+    )
+    ]
+    ,
+    )
+    ,
     );
   }
 }
